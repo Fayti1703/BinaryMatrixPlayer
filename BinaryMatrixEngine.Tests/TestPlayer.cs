a64c16eb8fe745452a20a3f5b5cfe2c42918bde7
Fayti1703 <fayti1703@protonmail.com>
2024-02-25 12:07:08 +0100

    Engine.Tests: Add game context test infrastructure

---
+++ BinaryMatrixEngine.Tests/TestPlayer.cs
@@ -0,0 +1,14 @@
+using Fayti1703.CommonLib;
+
+namespace BinaryMatrix.Engine.Tests;
+
+public class TestPlayerActor : PlayerActor {
+	public ActionSet? currentAction;
+	public OperationError lastOperationError = OperationError.NONE;
+
+	public void ReportOperationError(OperationError error) {
+		this.lastOperationError = error;
+	}
+
+	public void Dispose() { }
+}
