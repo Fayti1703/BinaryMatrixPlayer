470b75fd833cfeb93110e49be0a7c79d95a50057
Fayti1703 <fayti1703@protonmail.com>
2024-05-04 22:44:46 +0200

    Engine: Add an SFC32 RNG implementation

--- BinaryMatrixEngine/RNG.cs
+++ BinaryMatrixEngine/RNG.cs
@@ -13,3 +13,32 @@ public class RandomRNG : RNG {
 	}
 	public int Next(int upperBound) => this.random.Next(upperBound);
 }
+
+
+public class SFC32RNG : RNG {
+	private uint a, b, c, d;
+
+	public SFC32RNG(uint a, uint b, uint c, uint d = 1) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	public int Next(int upperBound) {
+		return (int) ((double) this.RawNext() / uint.MaxValue * upperBound);
+	}
+
+	public uint RawNext() {
+		uint t = this.a + this.b + this.d++;
+		this.a = this.b ^ this.b >>> 9;
+		this.b = this.c + (this.c << 3);
+		this.c = this.c << 21 | this.c >>> 11;
+		this.c += t;
+		return t;
+	}
+
+	public uint[] GetState() {
+		return [ this.a, this.b, this.c, this.d ];
+	}
+}
