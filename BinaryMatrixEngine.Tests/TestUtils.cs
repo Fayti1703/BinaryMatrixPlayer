973f5afa00d402f132060c592139b48d0fa8d74a
Fayti1703 <fayti1703@protonmail.com>
2024-11-02 16:00:59 +0100

    Add TestUtils.SetBoardLanes

--- BinaryMatrixEngine.Tests/TestUtils.cs
+++ BinaryMatrixEngine.Tests/TestUtils.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Fayti1703.CommonLib.Enumeration;
 using JetBrains.Annotations;
 
 namespace BinaryMatrix.Engine.Tests;
@@ -17,5 +19,20 @@ internal class TestUtils {
 	[MustDisposeResource]
 	internal static GameContext CreateStaticRNGScenarioContext(int[]? rngSequence = null) {
 		return CreateScenarioContext(new StaticRNG(rngSequence ?? Array.Empty<int>()));
+	}
+
+	internal static void SetBoardLanes(GameBoard target, IEnumerable<IEnumerable<string>> laneCards) {
+		for(int i = 0; i < 6; i++) {
+			target[CellName.L0 + i].cards.Clear();
+		}
+
+		foreach((int index, IEnumerable<string> cards) in laneCards.WithIndex()) {
+			new CardList(cards.Select((str, i) => {
+				if(str.Length != 2) throw new ArgumentException($"Invalid CardID at {index}:{i}", nameof(laneCards));
+				Value value = CardID.ValueFromSymbol(str[0]) ?? throw new ArgumentException($"Invalid Value in CardID at {index}:{i}", nameof(laneCards));
+				Axiom axiom = CardID.AxiomFromSymbol(str[1]) ?? throw new ArgumentException($"Invalid Axiom in CardID at {index}:{i}", nameof(laneCards));
+				return new Card(value, axiom);
+			})).MoveAllTo(target[CellName.L0 + index].cards);
+		}
 	}
 }
